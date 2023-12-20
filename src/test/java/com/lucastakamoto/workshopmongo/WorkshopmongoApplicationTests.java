f7e286b01ba492c6c161dee1af52187ecd07486b
Lucas Takamoto <lucas.takamoto@hotmail.com>
2023-12-19 22:11:32 -0300

    Projeto criado

---
+++ src/test/java/com/lucastakamoto/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucastakamoto.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
