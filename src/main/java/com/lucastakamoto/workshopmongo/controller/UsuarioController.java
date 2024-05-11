793f3f1d2884d1bf6de71985d698bffa0c4ea0e7
Lucas Takamoto <lucas.takamoto@hotmail.com>
2024-05-10 23:53:53 -0300

    Modificação na conexão com o MongoDB

--- src/main/java/com/lucastakamoto/workshopmongo/controller/UsuarioController.java
+++ src/main/java/com/lucastakamoto/workshopmongo/controller/UsuarioController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "users")
+@RequestMapping(value = "/users")
 public class UsuarioController {
 
     @Autowired
