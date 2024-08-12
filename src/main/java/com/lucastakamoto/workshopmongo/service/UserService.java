1945415602e0295bcee44a4e3dafc9d91408561d
Lucas Takamoto <lucas.takamoto@hotmail.com>
2024-08-12 16:11:56 -0300

    Inserção de um novo usuario

--- src/main/java/com/lucastakamoto/workshopmongo/service/UserService.java
+++ src/main/java/com/lucastakamoto/workshopmongo/service/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import com.lucastakamoto.workshopmongo.domain.User;
+import com.lucastakamoto.workshopmongo.dto.UserDTO;
 import com.lucastakamoto.workshopmongo.exception.ObjectNotFoundException;
 import com.lucastakamoto.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> user = repo.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
