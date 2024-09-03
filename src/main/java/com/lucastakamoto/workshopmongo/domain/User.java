8cbe721a041c2d98745e8a5a0afcbda0ee88c530
Lucas Takamoto <lucas.takamoto@hotmail.com>
2024-09-03 15:46:36 -0300

    Referenciando os posts do usuário

--- src/main/java/com/lucastakamoto/workshopmongo/domain/User.java
+++ src/main/java/com/lucastakamoto/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.lucastakamoto.workshopmongo.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection="user")
@@ -15,6 +18,12 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    // Instância Lista de Posts
+    @DBRef(lazy = true) // Referenciando uma coleção,garantindo que os posts serão carregados se forem acessados
+    private List<Post> posts = new ArrayList<>();
+
+
 
     public User() {
     }
@@ -48,6 +57,14 @@ public class User implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
